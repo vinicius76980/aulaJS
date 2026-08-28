7dd1fc1e938b1915b38db1b82aab80b88b624e74
VINICIUS DA CRUZ XAVIER <53853275818@info.local>
2026-08-28 17:51:10 -0300

    aulaJS

---
+++ aula js/lica.js
@@ -0,0 +1,40 @@
+//for (let i = 1; i <= 10; i++) {
+//console.log(i);
+//}
+
+//let numero = Number(prompt("Digite um número:"));
+
+//for (let i = 1; i <= 10; i++) {
+    //console.log(numero + " x " + i + " = " + (numero * i))}
+
+
+
+
+//let numeros = Number(prompt("Digite um número inteiro positivo:"));
+
+//let soma = 0;
+
+//for (let i = 1; i <= numeros; i++) {
+  //  soma += i;
+//}
+
+//console.log("A soma é: " + soma);
+
+
+//for (let i = 1; i <= 50; i++) {
+   // if (i % 2 === 0) {
+     //   console.log(i);
+   // }
+//}
+
+
+//let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+
+//let tentativa = Number(prompt("Adivinhe o número entre 1 e 100:"));
+
+//if (tentativa === numeroSecreto) {
+    //console.log("Parabéns! Você acertou!");
+//} else {
+ //   console.log("Você errou!");
+   // console.log("O número era: " + numeroSecreto);
+//}
